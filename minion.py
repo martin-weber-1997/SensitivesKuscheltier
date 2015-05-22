c9a55f858abb71dfa57bc8e28753f6e0f9bc304b
MartinWeber02 <martinweber1997@outlook.com>
2015-05-22 17:32:51 +0200

    Klassen hinzugefuegt

--- minion.py
+++ minion.py
@@ -3,6 +3,32 @@ __author__ = 'Martin Weber'
 from threading import Thread
 
 
-class minion(Thread):
+class TemperatureValues(Thread):
+
+    def __init__(self):
+        Thread.__init__(self)
+
+
+class AdxlValues(Thread):
+    def _init_self(self):
+        Thread.__init__(self)
+
+
+class NFCReader(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+
+class Buttons(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+
+class MovementValues(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+
+class BrightnessValues(Thread):
     def __init__(self):
         Thread.__init__(self)
