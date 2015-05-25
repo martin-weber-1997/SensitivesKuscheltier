78a14953a50284d675c2f4fd3f1b86890e6b48af
MartinWeber02 <martinweber1997@outlook.com>
2015-05-25 11:43:00 +0200

    Adxl erweitert

--- minion.py
+++ minion.py
@@ -1,6 +1,13 @@
+# coding=utf-8
 __author__ = 'Martin Weber'
 
+from time import sleep
 from threading import Thread
+
+import RPi.GPIO as GPIO
+from SensitivesKuscheltier.adxl345 import ADXL345
+
+
 
 
 # LED 3 beliebige GPIOS
@@ -9,18 +16,20 @@ LED2 = 13
 LED3 = 15
 
 
-#Helligkeitssensor 1 beliebiger GPIO Pin
+# Helligkeitssensor 1 beliebiger GPIO Pin
 BRIGHTNESS = 16
 
-#Taster linke Hand
+# Taster linke Hand
 LH = 18
 
-#Taster rechte Hand
+# Taster rechte Hand
 RH = 22
+
+# Helligkeitssensor
+PIR = 36
 
 
 class TemperatureValues(Thread):
-
     def __init__(self):
         Thread.__init__(self)
 
@@ -28,6 +37,22 @@ class TemperatureValues(Thread):
 class AdxlValues(Thread):
     def _init_self(self):
         Thread.__init__(self)
+        self.adxl = ADXL345()
+        self.values = self.adxl.getAxes(True)
+        self.checkValues()
+
+    def checkValues(self):
+        # Werte muessen noch probiert werden welche Richtig sind
+        grenze = 0.2
+        grenze2 = 1
+        grenze3 = 2
+        while True:
+            if (self.values['x'] > grenze or self.values['y'] > grenze or self.values['z'] > grenze):
+            #Sound abspielen
+            elif (self.values['x'] > grenze2 or self.values['y'] > grenze2 or self.values['z'] > grenze2):
+                #Anderen Sound abspielen
+                elif (self.values['x'] > grenze3 or self.values['y'] > grenze3 or self.values['z'] > grenze3):
+                #Anderen Sound abspielen
 
 
 class NFCReader(Thread):
@@ -40,9 +65,14 @@ class Buttons(Thread):
         Thread.__init__(self)
 
 
-class MovementValues(Thread):
-    def __init__(self):
-        Thread.__init__(self)
+    def detectMovement(channel):
+        if GPIO.input(PIR) == GPIO.HIGH:
+            # Sound abspielen
+            # Augen ändern?
+            sleep(20)
+
+    # Kommt noch in Methode ruft detectMovement auf falls Bewegungssensor ausschlaegt
+    GPIO.add_event_detect(PIR, GPIO.HIGH, callback=detectMovement(), bouncetime=1000)
 
 
 class BrightnessValues(Thread):
@@ -54,6 +84,15 @@ class LED(object):
     def __init__(self):
 
 
-class Sounds(object):
+    def playSounds(self, sound):
+
+    #Sound aufrufen
 
 
+    def init():
+        GPIO.setup(PIR, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(LED1, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(LED2, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(LED3, GPIO.OUT, initial=GPIO.LOW)
+
+        sounds = Sounds()
