cd8f10134c5f43eb6bbb6c0baec92d1c81396b09
Martin Weber <mweber02@student.tgm.ac.at>
2015-05-27 10:35:46 +0200

    Added Logics for Raspberry

--- minion.py
+++ minion.py
@@ -2,25 +2,22 @@
 __author__ = 'Martin Weber'
 
 
-#Teils Code von Erhard List, Walter Rafeiner-Magor benutzt
+# Teils Code von Erhard List, Walter Rafeiner-Magor benutzt
 
 from time import sleep
 from threading import Thread
 
 import RPi.GPIO as GPIO
 from adxl345 import ADXL345
-import mixer from pygame
 from mf2 import NFCReader
+import os
 
-
-
-
-MODE=None
-TEMP=None
-ADXL=None
-READER=None
-IFR=None
-BRIGHT= None
+MODE = None
+TEMP = None
+ADXL = None
+READER = None
+IFR = None
+BRIGHT = None
 
 
 
@@ -35,22 +32,23 @@ LED3 = 15
 
 
 # Taster linke Hand
-LH = 18
+LH = 36
 
 # Taster rechte Hand
-RH = 22
+RH = 37
 
 # Begegungssensor
-PIR = 36
+PIR = 16
 
 #Temperatursensor
 TMP = 7
 
+
 class TemperatureValues(Thread):
     def __init__(self):
         Thread.__init__(self)
-        self.closed=False
-        self.stopped=True
+        self.closed = False
+        self.stopped = True
         self.run()
 
     def run(self):
@@ -58,17 +56,22 @@ class TemperatureValues(Thread):
             while not self.stopped and not self.closed:
                 #Farben ausgeben
                 self.checkTemp()
+
     def checkTemp(self):
         #Farben ausgeben mit Temperatur
+        print"Farbe ausgeben"
 
 class Mode(object):
     def __init__(self):
         self.mode = 0
+        self.long_pressed = 1
+
     def change(self):
-        self.mode = (self.mode + 1) % 4
+        self.mode = (self.mode + 1) % 5
 
     def increase(self):
         self.long_pressed += 1
+
 
 class AdxlValues(Thread):
     def _init_self(self):
@@ -76,7 +79,7 @@ class AdxlValues(Thread):
         self.adxl = ADXL345()
         self.stopped = True
         self.closed = False
-        self.x ,self.y,self.z = 0, 0, 0
+        self.x, self.y, self.z = 0, 0, 0
         self.start()
 
     def run(self):
@@ -89,7 +92,8 @@ class AdxlValues(Thread):
         # Werte muessen noch probiert werden welche Richtig sind
         grenze = 0.2
         grenze2 = 1
-        self.x ,self.y,self.z = self.adxl.getAxes(True)['x'],self.adxl.getAxes(True)['y'],self.adxl.getAxes(True)['z']
+        self.x, self.y, self.z = self.adxl.getAxes(True)['x'], self.adxl.getAxes(True)['y'], self.adxl.getAxes(True)[
+            'z']
         while True:
             if (self.x > grenze or self.y > grenze or self.z > grenze):
                 # Sound abspielen
@@ -108,18 +112,18 @@ class NFCReader(Thread):
         self.start()
 
     def run(self):
-         while not self.closed:
+        while not self.closed:
             while not self.closed and not self.stopped:
-                if(GPIO.input(LH) == GPIO.HIGH):
+                if (GPIO.input(LH) == GPIO.HIGH):
+                    print"Hand gedrueckt"
                     sound = self.reader.run()
                     playSounds(sound)
-
-
-
+                    sleep(3)
+                sleep(0.1)
+                print"Test"
 
 
 class Infrarot(Thread):
-
     def __init__(self):
         Thread.__init__(self)
         self.stopped = True
@@ -129,10 +133,9 @@ class Infrarot(Thread):
     def run(self):
         while not self.closed:
             while not self.closed and not self.stopped:
-                if(GPIO.input(PIR) == GPIO.HIGH):
+                if (GPIO.input(PIR) == GPIO.HIGH):
                     playSounds("HelloPapagena")
                     sleep(10)
-
 
 
 class BrightnessValues(Thread):
@@ -141,6 +144,7 @@ class BrightnessValues(Thread):
         self.stopped = True
         self.closed = False
         self.start()
+
     def run(self):
         while not self.closed:
             while not self.closed and not self.stopped:
@@ -149,64 +153,75 @@ class BrightnessValues(Thread):
     def checkBrightness(self):
         #Wenn helligkeit unter X
         playSounds("Hello")
+        print"Helligkeit"
         sleep(5)
 
 
-
-
 def playSounds(self, sound):
-    if(sound=="ahh"):
+    if (sound == "ahh"):
         mixer.music.load("/home/team1/Audio/Ahh")
         mixer.music.play()
-    elif(sound=="401f8231"):
+    elif (sound == "401f8231"):
         #Apfel
-        mixer.music.load("/home/team1/Audio/Bappel")
-        mixer.music.play()
-    elif(sound=="Grapo"):
-        #NFC einfuegen
-        mixer.music.load("/home/team1/Audio/Grapo")
-        mixer.music.play()
-    elif(sound=="5dd2b12b"):
+        #mixer.music.load("/home/team1/Audio/Bappel")
+        #mixer.music.play()
+        os.system('sudo omxplayer --amp 2000 /home/team1/Audio/Bappel/1.wav')
+
+    elif (sound == "5dd2b12b"):
         #Kiwi
-        mixer.music.load("/home/team1/Audio/Kiwi")
-        mixer.music.play()
-    elif(sound=="443fa822"):
+        #mixer.music.load("/home/team1/Audio/Kiwi")
+        #mixer.music.play()
+        os.system('sudo omxplayer --amp 2000 /home/team1/Audio/Kiwi/1.wav')
+    elif (sound == "443fa822"):
         #Orange
-        mixer.music.load("/home/team1/Audio/Oranja")
-        mixer.music.play()
-    elif(sound=="b5e69220"):
+        #mixer.music.load("/home/team1/Audio/Oranja")
+        #mixer.music.play()
+        os.system('sudo omxplayer --amp 2000 /home/team1/Audio/Oranja/1.wav')
+    elif (sound == "b5e69220"):
         #Banane
-        mixer.music.load("/home/team1/Audio/Banana")
-        mixer.music.play()
-    elif(sound=="cd2bb22b"):
+        #mixer.music.load("/home/team1/Audio/Banana")
+        #mixer.music.play()
+        os.system('sudo omxplayer --amp 2000 /home/team1/Audio/Banana/1.wav')
+    elif (sound == "cd2bb22b"):
         #Melon
-        mixer.music.load("/home/team1/Audio/Melon")
-        mixer.music.play()
-    elif(sound=="Hello"):
-        mixer.music.load("/home/team1/Audio/Hello")
-        mixer.music.play()
-    elif(sound=="Popo"):
-        mixer.music.load("/home/team1/Audio/Popo")
-        mixer.music.play()
-    elif(sound=="TaDaa"):
-        mixer.music.load("/home/team1/Audio/TaDaa")
-        mixer.music.play()
-    elif(sound==""):
+        #mixer.music.load("/home/team1/Audio/Melon")
+        #mixer.music.play()
+        os.system('sudo omxplayer --amp 2000 /home/team1/Audio/Melon/1.wav')
+    elif (sound == ""):
         #NFC nicht erkannt
-        mixer.music.load("/home/team1/Audio/What")
-        mixer.music.play()
-    elif(sound=="beeDo"):
-        mixer.music.load("/home/team1/Audio/beeDo")
-        mixer.music.play()
-    elif(sound=="Haha"):
-        mixer.music.load("/home/team1/Audio/Haha")
-        mixer.music.play()
-    elif(sound=="HelloPapagena"):
-        mixer.music.load("/home/team1/Audio/HelloPapagena")
-        mixer.music.play()
-    elif(sound=="Poopaye"):
-        mixer.music.load("/home/team1/Audio/Stopa")
-        mixer.music.play()
+        #mixer.music.load("/home/team1/Audio/What")
+        #mixer.music.play()
+        os.system('sudo omxplayer --amp 2000 /home/team1/Audio/What/1.wav')
+    elif (sound == "Hello"):
+        #mixer.music.load("/home/team1/Audio/Hello")
+        #mixer.music.play()
+        os.system('sudo omxplayer --amp 2000 /home/team1/Audio/Hello/1.wav')
+    elif (sound == "Popo"):
+        #mixer.music.load("/home/team1/Audio/Popo")
+        #mixer.music.play()
+        os.system('sudo omxplayer --amp 2000 /home/team1/Audio/Popo/1.wav')
+    elif (sound == "TaDaa"):
+        #mixer.music.load("/home/team1/Audio/TaDaa")
+        #mixer.music.play()
+        os.system('sudo omxplayer --amp 2000 /home/team1/Audio/TaDaa/1.wav')
+    elif (sound == "beeDo"):
+        #mixer.music.load("/home/team1/Audio/beeDo")
+        #mixer.music.play()
+        os.system('sudo omxplayer --amp 2000 /home/team1/Audio/beeDo/1.wav')
+    elif (sound == "Haha"):
+        #mixer.music.load("/home/team1/Audio/Haha")
+        #mixer.music.play()
+        os.system('sudo omxplayer --amp 2000 /home/team1/Audio/Haha/1.wav')
+    elif (sound == "HelloPapagena"):
+        #mixer.music.load("/home/team1/Audio/HelloPapagena")
+        #mixer.music.play()
+        os.system('sudo omxplayer --amp 2000 /home/team1/Audio/HelloPapagena/1.wav')
+    elif (sound == "Poopaye"):
+        #mixer.music.load("/home/team1/Audio/Stopa")
+        #mixer.music.play()
+        os.system('sudo omxplayer --amp 2000 /home/team1/Audio/Stopa/1.wav')
+
+
 #
 #Methode noch nicht vollständig
 #
@@ -214,14 +229,18 @@ def playSounds(self, sound):
 #
 def button_pressed(channel):
     # if BUTTON was rising
+    print"Methode Aufgerufen"
     if GPIO.input(RH) == GPIO.HIGH:
         MODE.increase()
+        print"Test"
     else:
         # change to next mode
         MODE.change()
     # if button was long pressed for four times:
     if MODE.long_pressed >= 4:
         #herrunterfahren
+        print"Herrunterfahren"
+
     if MODE.mode == 1:
         #stop Brightness
         BRIGHT.stopped = True
@@ -241,28 +260,47 @@ def button_pressed(channel):
         ADXL.stopped = True
         #start NFC
         READER.stopped = False
-    elif MODE.mode == 0:
+    elif MODE.mode == 4:
         #Stop NFC
         READER.stopped = True
         #Start brightness
         BRIGHT.stopped = False
+    elif MODE.mode == 0:
+        BRIGHT.stopped=True
 
 
-
-
-
-    def init():
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(LED1, GPIO.OUT, initial=GPIO.LOW)
-        GPIO.setup(LED2, GPIO.OUT, initial=GPIO.LOW)
-        GPIO.setup(LED3, GPIO.OUT, initial=GPIO.LOW)
-        GPIO.setup(PIR, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-        GPIO.setup(RH, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-        GPIO.setup(LH, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-
-        GPIO.add_event_detect(RH, GPIO.BOTH, callback = button_pressed, bouncetime = 800)
+def init():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(LED1, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(LED2, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(LED3, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(PIR, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(RH, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(LH, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(RH, GPIO.BOTH, callback=button_pressed, bouncetime=800)
 
 
 if __name__ == "__main__":
-    MODE=Mode()
+    MODE = Mode()
+    TEMP = TemperatureValues()
+    ADXL = AdxlValues()
+    READER = NFCReader()
+    IFR = Infrarot()
+    BRIGHT = BrightnessValues()
+    init()
+
+try:
+    while True:  # endless looping
+        pass
+except KeyboardInterrupt:
+    TEMP.closed = True
+    MODE.closed = True
+    ADXL.closed = True
+    READER.closed = True
+    IFR.closed = True
+    BRIGHT.closed = True
+    # give me time to close all threads
+    sleep(1)
+finally:
+    closeGPIO()
 
